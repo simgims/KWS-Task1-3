58cbc4e1be70337a6bfe746a0f7c7d5888198a3a
Simen Gimsøy <simgims@hotmail.com>
2025-01-28 14:47:45 +0100

    Setup for github pages

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // Change "/my-repo" to be the name of your GitHub repository. For example
+  // when my code is at https://github.com/kristiania-kws2100-2025/kws2100-kartbaserte-websystemer/
+  // I should have `base: "/kws2100-kartbaserte-websystemer"`
+  base: "/KWS-Task1-3",
+});
