f178d4f3ec4f643df58224da7cbd4102956658e9
Simen Gimsøy <simgims@hotmail.com>
2025-01-28 16:11:06 +0100

    Checking if layers work

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function focusStyle(feature: FeatureLike) {
 
 const CountyLayer = new VectorLayer({
   source: new VectorSource({
-    url: "/geojson/fylker.json",
+    url: "KWS-Task1-3//fylker.json",
     format: new GeoJSON(),
   }),
   style: new Style({ stroke: new Stroke({ color: "red, width: 2" }) }),
@@ -37,7 +37,7 @@ const map = new Map({
     CountyLayer,
     new VectorLayer({
       source: new VectorSource({
-        url: "/geojson/VGS.json",
+        url: "KWS-Task1-3/VGS.json",
         format: new GeoJSON(),
       }),
     }),
